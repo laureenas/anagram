a8243cba74a924602e7f0003613d78b8714938e8
Laurynas Speičys <laurynas@pov.lt>
2023-09-10 23:21:24 +0300

    Add services helper models

---
+++ swagger_server/services/models.py
@@ -0,0 +1,56 @@
+LETTER_IDX_EN = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7,
+    'i': 8,
+    'j': 9,
+    'k': 10,
+    'l': 11,
+    'm': 12,
+    'n': 13,
+    'o': 14,
+    'p': 15,
+    'q': 16,
+    'r': 17,
+    's': 18,
+    't': 19,
+    'u': 20,
+    'v': 21,
+    'w': 22,
+    'x': 23,
+    'y': 24,
+    'z': 25,
+}
+LETTER_COUNT_EN = len(LETTER_IDX_EN)
+
+
+def get_letter_map(word):
+    '''Crate a 26 symbol string representing count of each letter in the word.
+
+    Assumption: no more than 9 identical letters in an EN word
+    (could be raised to 15 quite easily if need be).
+
+    Example:
+
+    'a' - '10000000000000000000000000'
+    'b' - '01000000000000000000000000'
+    'z' - '00000000000000000000000001'
+    'aab' - '21000000000000000000000000'
+
+    '''
+
+    if word is None:
+        return None
+
+    letter_map = [0]*LETTER_COUNT_EN
+    for letter in word:
+        if letter not in LETTER_IDX_EN:
+            raise ValueError(f'Not supported Letter {letter} in word {word}')
+        letter_map[LETTER_IDX_EN[letter]] += 1
+
+    return ''.join(map(str, letter_map))
--- swagger_server/tests/services/test_models.py
+++ swagger_server/tests/services/test_models.py
@@ -1,11 +1,28 @@
-from test import services
+import pytest
+
+from services.models import LETTER_IDX_EN, get_letter_map
 
 
 def test_get_letter_map():
     result = get_letter_map('dear')
-    assert result == ''
+    assert result == '10011000000000000100000000'
 
 
-def test_get_letter_map_emppty():
-    result = get_letter_map()
+def test_get_letter_map_empty():
+    result = get_letter_map(None)
     assert result is None
+
+
+def test_get_letter_map_empty_string():
+    result = get_letter_map('')
+    assert result == '00000000000000000000000000'
+
+
+def test_get_letter_map_multiple_letters():
+    result = get_letter_map('door')
+    assert result == '00010000000000200100000000'
+
+
+def test_get_letter_map_letter_idx():
+    result = get_letter_map(''.join(LETTER_IDX_EN.keys()))
+    assert result == '11111111111111111111111111'
