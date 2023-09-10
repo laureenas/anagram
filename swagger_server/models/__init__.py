8d28f948e77fe49640a7b6b9398074611cee2f64
Laurynas Speičys <laurynas@pov.lt>
2023-09-10 23:14:02 +0300

    Get rid of a few leftwovers from boilerplate

--- swagger_server/models/__init__.py
+++ swagger_server/models/__init__.py
@@ -1,7 +1,5 @@
 # coding: utf-8
 
-# flake8: noqa
-from __future__ import absolute_import
 # import models into model package
 from models.anagram_list import AnagramList
 from models.inline_response200 import InlineResponse200
