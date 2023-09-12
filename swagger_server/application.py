e77684a2a72d988d4097bb9666a38a13b1df114a
Laurynas Speičys <laurynas@pov.lt>
2023-09-12 10:53:36 +0300

    Refactoring: cleare distiction between connextion_app and flask_app

--- swagger_server/application.py
+++ swagger_server/application.py
@@ -12,37 +12,36 @@ db = SQLAlchemy()
 
 
 def create_application():
-    app = connexion.App(__name__, specification_dir='./swagger/')
+    connexion_app = connexion.App(__name__, specification_dir='./swagger/')
 
     # This seems to be needed by connexion
-    app.app.json_encoder = FlaskJSONEncoder
+    connexion_app.json_encoder = FlaskJSONEncoder
+    flask_app = connexion_app.app
 
     # Enable API 'mode' of connexion
-    app.add_api('swagger.yaml', arguments={'title': 'Simple Anagram API'},
-                pythonic_params=True, validate_responses=True)
+    connexion_app.add_api('swagger.yaml', arguments={'title': 'Simple Anagram API'},
+                          pythonic_params=True, validate_responses=True)
 
     # Configure the Flask application
     config_type = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
-    app.app.config.from_object(config_type)
-    app.app.logger.info(f'Reading config {config_type} done...')
+    flask_app.config.from_object(config_type)
+    flask_app.logger.info(f'Reading config {config_type} done...')
 
     # Initialize DB
-    db.init_app(app.app)
+    db.init_app(flask_app)
     # Check if the DB schema needs to be initialized
     from models.corpus import Corpus
 
-    engine = sa.create_engine(app.app.config['SQLALCHEMY_DATABASE_URI'])
+    engine = sa.create_engine(flask_app.config['SQLALCHEMY_DATABASE_URI'])
     inspector = sa.inspect(engine)
-    if not inspector.has_table("corpus"):
-        with app.app.app_context():
+    if not inspector.has_table('corpus'):
+        with flask_app.app_context():
             db.drop_all()
             db.create_all()
-            app.app.logger.info('Database initialization done...')
+
+            flask_app.logger.info('Database initialization done...')
     else:
-        app.app.logger.info('Database already initialized - skipping initialization')
-
-    return app
-
+        flask_app.logger.info('Database already initialized - skipping initialization')
 
 def run_application():
     '''Run application in dev mode. No WSGI.'''
